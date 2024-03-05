cca8eb49c2b7813db830221413b5f3a790ba40cc
trung6755 <trungggrr@gmail.com>
2024-03-05 15:38:48 +0700

    Initial commit

---
+++ Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace animal
+{
+    public abstract class Animal
+    {
+        protected double weight { get; set; }
+        protected string color { get; set; }
+        
+        public Animal()
+        {
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public abstract void printInfo();
+    }
+
+    public class Cat : Animal
+    {
+        private string name {  get; set; }
+        public Cat(double weight, string color, string name)
+        {
+            this.weight = weight;
+            this.color = color;
+            this.name = name;
+        }
+        public override void printInfo()
+        {
+            Console.WriteLine("Weight: {0}" + '\n' + "Color: {1}" + '\n' + "Name: {2}", this.weight, this.color, this.name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat(5, "blue", "Mew");
+            cat.printInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
